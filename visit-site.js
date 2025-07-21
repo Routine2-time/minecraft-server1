2ea7536b0b935a34e277d6e5f6f774d05aa81191
iizukar <czechia.deitan@gmail.com>
2025-07-21 22:25:25 +0200

    Update visit-site.js

--- visit-site.js
+++ visit-site.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   await page.goto('http://www-microsoft.iceiy.com/', { waitUntil: 'networkidle2', timeout: 60000 });
   console.log('Visited http://www-microsoft.iceiy.com/');
