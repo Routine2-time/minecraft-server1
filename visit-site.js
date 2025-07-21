682c8ce71a07041d9f0a930bd571b6595801c683
iizukar <czechia.deitan@gmail.com>
2025-07-21 22:27:27 +0200

    Update visit-site.js

--- visit-site.js
+++ visit-site.js
@@ -7,6 +7,7 @@ const puppeteer = require('puppeteer');
   });
   const page = await browser.newPage();
   await page.goto('http://www-microsoft.iceiy.com/', { waitUntil: 'networkidle2', timeout: 60000 });
-  console.log('Visited http://www-microsoft.iceiy.com/');
-  await browser.close();
+  console.log('Visited http://www-microsoft.iceiy.com/ and keeping browser open.');
+  // Keep the browser and page open indefinitely
+  await new Promise(() => {});
 })();
