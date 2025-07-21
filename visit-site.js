a01dcdc88553d7152102fcf328319d405a3243a5
iizukar <czechia.deitan@gmail.com>
2025-07-21 22:22:27 +0200

    Create visit-site.js

---
+++ visit-site.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto('http://www-microsoft.iceiy.com/', { waitUntil: 'networkidle2', timeout: 60000 });
+  console.log('Visited http://www-microsoft.iceiy.com/');
+  await browser.close();
+})();
